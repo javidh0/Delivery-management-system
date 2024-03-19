75d6c2ccffa0e0a0deb80b464914fdeda6a0fd91
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-03-19 20:00:44 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const {getDbConn, conntectDb} = require('./database');
+const {ROLES, DB_ACCESS} = require("./authorization/roles.js");
 
 const app = express();
 app.use(express.json());
+let conn;
 
-app.listen(1729, () => {
-    console.log(`Successfully started server on port 1729.`);
-});
+function server(error){
+  if(!error){
+    conn = getDbConn();
+    app.listen(1729, () => {
+        console.log(`Successfully started server on port 1729.`);
+    });
+  }
+}
+
+conntectDb(server);
 
 app.get('/', (req, res) => {
-  res.send("helloo")
+  conn.collection('users').findOne({'name':'javidh'}, {'roles':1})
+  .then(val => {
+    const role = val.roles;
+    console.log(ROLES[role]);
+  });
+  res.send("hello");
 })
