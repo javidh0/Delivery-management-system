f3df27cd572662ea29ace0b81dcb144482f08104
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-03-25 13:05:10 +0530

    Update authenticate.js

--- src/authentication/authenticate.js
+++ src/authentication/authenticate.js
@@ -1,6 +1,39 @@
+function validateAuthentication(conn, user_id, token, next){
+    conn.collection('access_tokens').findOne({'user_id':user_id})
+    .then(val => {
+        if(val != null){
+            if(val['_id'] == token.toString()){
+                let time = new Date()
+                time.setMinutes(time.getMinutes()-5);
+                if(val['time'] < time){ 
+                    next("time limit ex");
+                    conn.collection('access_tokens').deleteOne({'user_id':user_id});
+                }else{
+                    next(val['_id'].toString());
+                }
+            }else{
+                next('-1');
+            }
+        }else{
+            next('-1');
+        }
+    })
+}
 
-function generateAccessToken(){
-
+function generateAccessToken(conn, user, next){
+    conn.collection('access_tokens').findOne({'user_id':user})
+    .then(val => {
+        if(val != null) {
+            return next(val['_id'].toString());
+        }else{
+            conn.collection('access_tokens').insertOne({'user_id': user, 'time':new Date()})
+            .then((val) => {
+                next(val['insertedId'].toString());
+            }).catch((err)=>{
+                next(err);
+            })
+        }
+    });
 }
 
 function authenticateUser(data, conn, next){
@@ -12,15 +45,17 @@ function authenticateUser(data, conn, next){
                 data.cred[0] == val.cred[0] 
                 && data.cred[1] == val.cred[1]
                 ){  
-                 console.log("succesfull");
-                 next();
+                    generateAccessToken(conn, data.user_id, next)
                 }
-            else console.log("failed");
+            else{ 
+                return next('-1');
+            }
         }
     )
 }
 
 module.exports = {
     authenticateUser, 
-    generateAccessToken
+    generateAccessToken,
+    validateAuthentication,
 }
