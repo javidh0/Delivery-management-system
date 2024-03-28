8773a9611f4febc285df927673983fd5aa2b3f5e
javidh0 <120474876+javidh0@users.noreply.github.com>
2024-03-28 20:42:30 +0530

    Update authorization.js

--- src/authorization/authorization.js
+++ src/authorization/authorization.js
@@ -32,6 +32,37 @@ function authorizeUserRole(conn, user_id, role, token, next) {
     )
 }
 
+function authorizeUserAccess(conn, user_id, role, token, next) {
+    validateAuthentication(
+        conn, user_id, token,
+        (val) => {
+            if(val == '-1') return next({
+                'status' : '-1',
+                'reason' : 'NSI',
+            })
+            else {
+                conn.collection('users').findOne({'user_id' : user_id})
+                .then(
+                    (val) => {
+                        if(ROLES[val['roles']] == role) {
+                            return next({
+                                'status' : '1',
+                                'role' : role,
+                            })
+                        }
+                        else {
+                            return next({
+                                'status' : '-1',
+                                'reason' : 'NA',
+                            })
+                        }
+                    }
+                )
+            }
+        }
+    )
+}
+
 module.exports = {
     authorizeUserRole,
 }
